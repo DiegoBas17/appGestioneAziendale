f74337aa78778f58073ca95263aef9186b8a4c57
Tigran <137753420+TigranPr@users.noreply.github.com>
2025-01-20 14:36:18 +0100

    Update AppGestioneAziendaleApplication.java

--- src/main/java/com/example/appGestioneAziendale/AppGestioneAziendaleApplication.java
+++ src/main/java/com/example/appGestioneAziendale/AppGestioneAziendaleApplication.java
@@ -8,7 +8,7 @@ public class AppGestioneAziendaleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppGestioneAziendaleApplication.class, args);
-		System.out.println("Sono Tigran");
+		System.out.println("Sono Fabio");
 	}
 
 }
