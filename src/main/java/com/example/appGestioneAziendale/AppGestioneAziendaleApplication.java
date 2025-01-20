a73270c69e18163551d471ef77087f015361b3f9
DiegoBas17 <d.basili17@gmail.com>
2025-01-20 14:17:06 +0100

    Initial commit

---
+++ src/main/java/com/example/appGestioneAziendale/AppGestioneAziendaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.appGestioneAziendale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGestioneAziendaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGestioneAziendaleApplication.class, args);
+	}
+
+}
