53ddde53775a7c1984dee8e79529e671e5a0fd07
DiegoBas17 <d.basili17@gmail.com>
2025-01-21 12:38:47 +0100

    create entità dipendente e comune

--- src/main/java/com/example/appGestioneAziendale/domain/entities/Dipendente.java
+++ src/main/java/com/example/appGestioneAziendale/domain/entities/Dipendente.java
@@ -1,10 +1,15 @@
 package com.example.appGestioneAziendale.domain.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -15,4 +20,37 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @Table(name = "dipendente")
 @EntityListeners(AuditingEntityListener.class)
 public class Dipendente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String cognome;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @ManyToOne(optional = false)
+    @Column(name = "comune_di_nascita", nullable = false)
+    private Comune comuneDiNascita;
+    @Column(name = "data_nascita", nullable = false)
+    private LocalDate dataDiNascita;
+    @Column(nullable = false)
+    private String telefono;
+    @Column(nullable = false, columnDefinition = "LONGTEXT")
+    private String avatar;
+    //private PosizioneLavorativa posizioneLavorativa;
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @CreatedBy
+    @Column(name = "created_by")
+    private Long createdBy;
+    @LastModifiedDate
+    @Column(name = "last_modified_at")
+    private LocalDateTime lastModifiedAt;
+    @LastModifiedBy
+    @Column(name = "last_modified_by")
+    private Long lastModifiedBy;
 }
