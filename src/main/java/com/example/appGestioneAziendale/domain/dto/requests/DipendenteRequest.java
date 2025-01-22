45d6580d201dac43c866345da550921403352067
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2025-01-22 15:08:58 +0100

    aggiunto hendler per la cattura degli errori e modifiche dei service di dipendente e comune

--- src/main/java/com/example/appGestioneAziendale/domain/dto/requests/DipendenteRequest.java
+++ src/main/java/com/example/appGestioneAziendale/domain/dto/requests/DipendenteRequest.java
@@ -1,19 +1,30 @@
 package com.example.appGestioneAziendale.domain.dto.requests;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 
 import java.time.LocalDate;
 
 public record DipendenteRequest(
         @NotBlank(message = "Il nome non può essere blank o null")
         String nome,
+        @NotBlank(message = "Il cognome non può essere blank o null")
         String cognome,
+        @Email(message = "Email non valida")
         String email,
+        @NotBlank(message = "La password non può essere blank o null")
         String password,
+        @NotNull(message = "Comune non presente")
         EntityIdRequest comune_di_nascita,
+        @Past
         LocalDate data_di_nascita,
+        @NotBlank(message = "Il numero di telefono non può essere blank o null")
         String telefono,
+        @NotBlank(message = "Problemi con l'Avatar")
         String avatar,
+        @NotBlank(message = "Il ruolo non può essere blank o null")
         String ruolo
 ) {
 }
--- src/main/java/com/example/appGestioneAziendale/services/ComuneService.java
+++ src/main/java/com/example/appGestioneAziendale/services/ComuneService.java
@@ -1,7 +1,23 @@
 package com.example.appGestioneAziendale.services;
 
+import com.example.appGestioneAziendale.domain.dto.requests.ComuneRequest;
+import com.example.appGestioneAziendale.domain.entities.Comune;
+import com.example.appGestioneAziendale.domain.exceptions.MyEntityNotFoundException;
+import com.example.appGestioneAziendale.repository.ComuneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ComuneService {
+    @Autowired
+    private ComuneRepository comuneRepository;
+
+    public Comune saveComune(ComuneRequest newComune) {
+        return Comune.builder().nome(newComune.nome()).regione(newComune.regione()).build();
+    }
+
+    public Comune getById(Long id) throws MyEntityNotFoundException {
+        return comuneRepository.findById(id)
+                .orElseThrow(() -> new MyEntityNotFoundException("Comune non trovato"));
+    }
 }
