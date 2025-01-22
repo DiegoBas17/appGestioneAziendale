1e29e26d6f4eb0695b91aab6c08cc7fc69587d20
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2025-01-22 14:34:43 +0100

    aggiunta dependency e il dto di dipendente request

--- src/main/java/com/example/appGestioneAziendale/domain/dto/requests/DipendenteRequest.java
+++ src/main/java/com/example/appGestioneAziendale/domain/dto/requests/DipendenteRequest.java
@@ -1,4 +1,19 @@
 package com.example.appGestioneAziendale.domain.dto.requests;
 
-public class DipendenteRequest {
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.LocalDate;
+
+public record DipendenteRequest(
+        @NotBlank(message = "Il nome non può essere blank o null")
+        String nome,
+        String cognome,
+        String email,
+        String password,
+        EntityIdRequest comune_di_nascita,
+        LocalDate data_di_nascita,
+        String telefono,
+        String avatar,
+        String ruolo
+) {
 }
