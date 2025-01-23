5d0ace4c392f2fd68fb101160fe1e3aeb14d00cf
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2025-01-23 12:02:02 +0100

    aggiunto metodo findbyid a dipendente service

--- src/main/java/com/example/appGestioneAziendale/services/DipendenteService.java
+++ src/main/java/com/example/appGestioneAziendale/services/DipendenteService.java
@@ -1,7 +1,17 @@
 package com.example.appGestioneAziendale.services;
 
+import com.example.appGestioneAziendale.domain.entities.Dipendente;
+import com.example.appGestioneAziendale.domain.exceptions.MyEntityNotFoundException;
+import com.example.appGestioneAziendale.repository.DipendenteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DipendenteService {
+    @Autowired
+    private DipendenteRepository dipendenteRepository;
+
+    public Dipendente getById(Long id) {
+        return dipendenteRepository.findById(id).orElseThrow(() -> new MyEntityNotFoundException("Dipendente con id: " + id + " non trovato!"));
+    }
 }
