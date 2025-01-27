a01bc2ad570dc6fe8e4adb8a937f9e1587fb4ef7
Tigran <137753420+TigranPr@users.noreply.github.com>
2025-01-27 11:42:40 +0100

    risolto problema nel global

--- src/main/java/com/example/appGestioneAziendale/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/appGestioneAziendale/handler/GlobalExceptionHandler.java
@@ -43,11 +43,7 @@ public class GlobalExceptionHandler {
                         .build());
     }
 
-    //In questo modo, se un doppio like viene tentato, il cliente riceve un messaggio più chiaro.
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
-    }
+
 
 
 }
